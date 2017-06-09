6cc082f7b2e0499a2383fb1dcb6bd7391c693ce4
ft115637850 <gentleman000000@hotmail.com>
2017-06-09 17:36:26 +0800

    upload project

---
+++ build.sbt
@@ -0,0 +1,19 @@
+name := "ClusterPoC"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+lazy val akkaVersion = "2.4.14"
+
+fork in Test := true
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-http" % "10.0.6",
+  "com.typesafe.akka" %% "akka-cluster" % akkaVersion,
+  "com.typesafe.akka" %% "akka-cluster-tools" % akkaVersion,
+  "com.typesafe.akka" %% "akka-persistence" % akkaVersion,
+  "org.iq80.leveldb" % "leveldb" % "0.7",
+  "org.fusesource.leveldbjni" % "leveldbjni-all" % "1.8",
+  "com.typesafe.akka" %% "akka-testkit" % akkaVersion % "test",
+  "org.scalatest" %% "scalatest" % "2.2.4" % "test",
+  "commons-io" % "commons-io" % "2.4" % "test")
---
+++ src/main/scala/ClusterNode.scala
@@ -0,0 +1,37 @@
+package cep.cluster.poc
+
+import java.util.UUID
+
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import akka.cluster.client.ClusterClient.SendToAll
+import cep.cluster.poc.AutomationRunner.RunAutomation
+import cep.cluster.poc.RegisterNodeProtocol.RegisterNode
+import com.google.common.util.concurrent.AbstractScheduledService.Scheduler
+import scala.concurrent.duration._
+/**
+  * Created by WH1409013 on 2017/6/9.
+  */
+
+object ClusterNode {
+
+  def props(registryProxy: ActorRef, registerInterval: FiniteDuration = 10.seconds): Props =
+    Props(classOf[ClusterNode], registryProxy, registerInterval)
+
+  case class WorkComplete(result: Any)
+}
+class ClusterNode(registryProxy: ActorRef, registerInterval: FiniteDuration) extends Actor with ActorLogging {
+  val NodeId = UUID.randomUUID().toString
+  import context.dispatcher
+  val registerTask = context.system.scheduler.schedule(0.seconds, registerInterval, registryProxy,
+    RegisterNode(NodeId))
+
+  override def receive: Receive = {
+    case Work(workId) =>
+      val runerId = scala.util.Random.alphanumeric.take(8).mkString
+      val aRunner = context.system.actorOf(AutomationRunner.prop, runerId)
+      aRunner ! RunAutomation(workId)
+    case WorkComplete(_, workId) =>
+      registryProxy ! WorkComplete(NodeId, workId)
+      log.info("work {} is complete on Node {}", workId, NodeId)
+  }
+}
